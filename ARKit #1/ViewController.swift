56c1e585819f741092949cfc883f3379018508b0
soareswallace <wssj2@cin.ufpe.br>
2018-07-01 19:08:17 -0300

    Removing box

--- ARKit #1/ViewController.swift
+++ ARKit #1/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         addBox()
+        addTapGestureToSceneView()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -30,16 +31,30 @@ class ViewController: UIViewController {
     }
     
     func addBox(){
-        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius:0)
+        //we are setting the box size -> 1.0 float is 1meter
         
         let boxNode = SCNNode()
         boxNode.geometry = box
         boxNode.position = SCNVector3(0, 0, -0.2) //stay on center with a little proximity to the camera
         //a rot node in a scnee that defines the coordinate system of the real world rendered by SceneKit
         
-        let scene = SCNScene()
-        scene.rootNode.addChildNode(boxNode)
-        sceneView.scene = scene
+        sceneView.scene.rootNode.addChildNode(boxNode)
+    }
+    
+    @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer){
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation)
+        guard let node = hitTestResults.first?.node else{
+            return
+        }
+        node.removeFromParentNode()
+    }
+    
+    func addTapGestureToSceneView(){
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+        
     }
 
 }
