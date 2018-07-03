a70d233c48d649bbf98a456948f94f1f545bb87c
soareswallace <wssj2@cin.ufpe.br>
2018-07-02 21:38:27 -0300

    basic movements

--- ARKit #1/ViewController.swift
+++ ARKit #1/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var sceneView: ARSCNView!
     let drone = Drone()
+    let kMovingLengthPerLoop: CGFloat = 0.05
+    let kRotationRadianPerLoop: CGFloat = 0.2
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +41,48 @@ class ViewController: UIViewController {
     func addDrone(){
         drone.loadModel()
         sceneView.scene.rootNode.addChildNode(drone)
+    }
+    
+    @IBAction func upLongPressed(_ sender: UILongPressGestureRecognizer) {
+        let action = SCNAction.moveBy(x: 0, y: kMovingLengthPerLoop, z: 0, duration: 0.1)
+        execute(action: action, sender: sender)
+    }
+    
+    @IBAction func downLongPressed(_ sender: UILongPressGestureRecognizer) {
+        let action = SCNAction.moveBy(x: 0, y: -kMovingLengthPerLoop, z: 0, duration: 0.1)
+        execute(action: action, sender: sender)
+    }
+    
+    @IBAction func moveRightLongPRessed(_ sender: UILongPressGestureRecognizer) {
+        let x = deltas().cos
+        let z = -deltas().sin
+        moveDrone(x: x, z: z, sender: sender)
+    }
+    
+    @IBAction func moveLeftLongPressed(_ sender: UILongPressGestureRecognizer) {
+        let x = -deltas().cos
+        let z = -deltas().sin
+        moveDrone(x: x, z: z, sender: sender)
+    }
+    
+    private func deltas() -> (sin: CGFloat, cos: CGFloat){
+    return (sin: kMovingLengthPerLoop * CGFloat(sin(drone.eulerAngles.y)), cos: kMovingLengthPerLoop * CGFloat(cos(drone.eulerAngles.y)))
+    }
+    
+    private func moveDrone(x: CGFloat, z: CGFloat, sender: UILongPressGestureRecognizer){
+        let action = SCNAction.moveBy(x: x, y: 0, z: z, duration: 0.1)
+        execute(action: action, sender: sender)
+    }
+    
+    
+    private func execute(action: SCNAction, sender: UILongPressGestureRecognizer){
+        let loopAction = SCNAction.repeat(action, count: 1)
+        if sender.state == .began{
+            drone.runAction(loopAction)
+        }
+        else if sender.state == .ended{
+            drone.removeAllActions()
+        }
     }
 }
 
